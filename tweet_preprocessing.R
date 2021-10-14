cb24b3d3381e93a836ebf3ed4f40cb4d4a9f5dfb
paldhous <peter@peteraldhous.com>
2021-10-14 14:17:16 -0700

    tweak to code to make timezone change clearer

--- tweet_preprocessing.R
+++ tweet_preprocessing.R
@@ -13,13 +13,13 @@ flagged1 <- flagged1 %>%
 flagged2 <- read_csv("data/CSVs/Raw/hobbs_flagler_unique.csv", cols(.default = col_character()), col_names = TRUE)
 # time_posted in this data appears to be in local time
 
-# create a tibble with timezones for hobbs and flagler, join tp flagged2 and force to UTC
+# create a tibble with timezones for hobbs and flagler, join to flagged2 and force to UTC
 timezones <- tibble(district = c("hobbs_nm","flagler_fl"), timezone = c("America/Denver","America/New_York"))
 flagged2 <- inner_join(flagged2,timezones) %>%
   mutate(created_at = mdy_hm(time_posted),
          created_at = case_when(grepl("PM",time_posted) ~ created_at + 0.5, # adding half a day for PM
                                 TRUE ~ created_at),
-         created_at = force_tzs(created_at,timezone))
+         created_at = force_tzs(created_at,timezone,tzone_out = "UTC"))
 
 # find the dupes with an inner join on district and created_at
 duplicated_tweets <- inner_join(flagged1,flagged2, by = c("district","created_at")) %>%
